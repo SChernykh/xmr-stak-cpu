d181210175308f6fa505d75a300b5ebaa9b1dd6f
SChernykh <sergey.v.chernykh@gmail.com>
2018-10-19 23:26:51 +0200

    Another asm code for Bullldozer
    Unoptimized, not sure which one is faster.

--- minethd.cpp
+++ minethd.cpp
@@ -346,7 +346,7 @@ bool minethd::self_test()
 
 			if (i > 0)
 			{
-				for (int j = 1; j <= 3; ++j)
+				for (int j = 1; j <= 4; ++j)
 				{
 					char hash[32];
 					cn_hash_fun hash_fun = func_selector(jconf::inst()->HaveHardwareAes(), i, j);
@@ -488,6 +488,7 @@ extern "C"
 	void cnv2_mainloop_ivybridge_asm(cryptonight_ctx* ctx0);
 	void cnv2_mainloop_ryzen_asm(cryptonight_ctx* ctx0);
 	void cnv2_mainloop_bulldozer_asm(cryptonight_ctx* ctx0, const uint32_t* sqrt_lut);
+	void cnv2_mainloop_bulldozer_2_asm(cryptonight_ctx* ctx0, const uint32_t* sqrt_lut);
 	void cnv2_double_mainloop_sandybridge_asm(cryptonight_ctx* ctx0, cryptonight_ctx* ctx1);
 	void cnv1_mainloop_soft_aes_sandybridge_asm(cryptonight_ctx* ctx0);
 	void cnv2_mainloop_soft_aes_sandybridge_asm(cryptonight_ctx* ctx0);
@@ -554,8 +555,10 @@ void cryptonight_hash_v2_asm(const void* input, size_t len, void* output, crypto
 		cnv2_mainloop_ivybridge_asm(ctx0);
 	else if (asm_version == 2)
 		cnv2_mainloop_ryzen_asm(ctx0);
-	else
+	else if (asm_version == 3)
 		cnv2_mainloop_bulldozer_asm(ctx0, SqrtV2Table);
+	else
+		cnv2_mainloop_bulldozer_2_asm(ctx0, SqrtV2Table);
 #ifdef PERFORMANCE_TUNING
 	t2 = __rdtsc();
 #endif
@@ -649,6 +652,8 @@ minethd::cn_hash_fun minethd::func_selector(bool bHaveAes, int variant, int asm_
 				// AMD Bulldozer
 				if (asm_version == 3)
 					return cryptonight_hash_v2_asm<3>;
+				if (asm_version == 4)
+					return cryptonight_hash_v2_asm<4>;
 			}
 		}
 	}
